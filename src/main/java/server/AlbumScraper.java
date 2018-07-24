0737133d8d5a1108b83cb62916717ed467bb0a22
Amy Cohen <anbcohen@gmail.com>
2018-07-24 11:06:49 -0700

    andrew lloyd weber's picture appears!

---
+++ src/main/java/server/AlbumScraper.java
@@ -0,0 +1,35 @@
+package server;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class AlbumScraper {
+    public static void main(String[] args) {
+        getAlbumArtURL("spice girls");
+    }
+
+    public static String getAlbumArtURL(String query) {
+        try {
+            String url = " https://www.discogs.com/search/?q=" + query + "&type=all";
+            Document doc = Jsoup.connect(url).get();
+
+            Elements albumCovers = doc.select(".thumbnail_center");
+            Element span = albumCovers.get(0);
+            Element img = span.child(0);
+            String src = img.attr("data-src");
+            System.out.println(src);
+            return src;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            return "/Users/amycohen/codefellows/401/lab-amy/10-album-art-server/src/main/resources/no-record-found.jpg";
+        }
+        return "/Users/amycohen/codefellows/401/lab-amy/10-album-art-server/src/main/resources/no-record-found.jpg";
+    }
+}
--- src/main/java/server/HomeController.java
+++ src/main/java/server/HomeController.java
@@ -1,11 +1,10 @@
 package server;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -14,24 +13,55 @@ public class HomeController {
     public String homePage () {
         return "index";
     }
-//https://www.discogs.com/search/?q=thrice+vheissu&type=all
 
-//    @RequestMapping("search/?/q=/{query}")
-//    @ResponseBody
+//    @RequestMapping("search/?/q=/{query}&type=all")
 //    public String querySearch (
 //      @PathVariable("query") String query
 //    ) {
-//        return query;
+//        return "cover";
 //    }
 
+
+    /*
+    ======================================================
+    WHERE I NEED TO SEARCH:
+    https://www.discogs.com/search/?q=andrew+lloyd+webber&type=all
+
+    IMG SRC NEEDED:
+    https://img.discogs.com/40VgAD5aipkblycrQSI0-w07IE4=/300x300/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-84839-1473273545-9408.jpeg.jpg
+
+    ======================================================
+
+    WHAT I'M GETTING BACK:  *search*
+    http://localhost:8080/search?q=andrew+lloyd+webber
+
+    WHAT I'M GETTING BACK: *querySearch*
+    http://localhost:8080/search?%2Fq=andrew+lloyed+webber
+    ======================================================
+    */
+
+    //From Steve's slides
     @RequestMapping("/search")
-    public String search(
-            @RequestParam("query") String query,
+    public String searchTemplate(
+            @RequestParam(name="q", defaultValue="The Beatles") String query,
             Model model
     ) {
-        model.addAttribute("artQuery", query);
+        String src = AlbumScraper.getAlbumArtURL(query);
+        model.addAttribute("query", query);
+        model.addAttribute("src", src);
         return "cover";
     }
+
+//    @RequestMapping("/search")
+//    public String search(
+//            @RequestParam("q") String query,
+////            @RequestParam("src") String src,
+//            Model model
+//    ) {
+//        model.addAttribute("query", query);
+////        model.addAttribute("src", src);
+//        return "cover";
+//    }
 
     //pulled from Steve's slides
     @RequestMapping("*")
