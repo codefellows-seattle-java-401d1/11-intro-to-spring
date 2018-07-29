1b1c97173b1137969d1d24e15c7331d3323cafff
Tara Johnson <tara@madface.com>
2018-07-28 20:23:25 -0700

    Discogs converted to use Spring.

--- src/main/java/server/HomeController.java
+++ src/main/java/server/HomeController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "Hello World!";
+    public String searchForm () {
+        return "index";
     }
+
+    @RequestMapping("/search")
+    public String searchResults(
+        @RequestParam(name="query", defaultValue = "Tom Petty") String query,
+        Model model
+    ) {
+        String src = AlbumScraper.getAlbumArtUrl(query);
+        model.addAttribute("query", query);
+        model.addAttribute("src", src);
+        return "result";
+    }
+
+    @RequestMapping("*")
+    public String notFound() {
+        return "404";
+    }
+
 }
