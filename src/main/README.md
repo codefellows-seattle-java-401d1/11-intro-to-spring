20bf0e71e617d79e615e43632fe7c0894d82a11f
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-28 22:50:34 -0700

    built and fully constructed project with all build.gradle dependencies. have code just like combination of slides and lecture and my controller can't find my templates. Can't find why. WHY? Google searches on the error have little results. Saving progress.

---
+++ src/main/README.md
@@ -0,0 +1,2 @@
+
+This site allows you to search for an artist or album and displays the first album cover result.
---
+++ src/main/java/server/AlbumScraper.java
@@ -0,0 +1,43 @@
+package server;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class AlbumScraper {
+
+    public static String query = "";
+    public static final String URL_ROOT = "https://www.discogs.com/";
+
+    public static void main(String[] args) {
+        getAlbumArt("beastie boys");
+    }
+
+    public static String getAlbumArt(String query) {
+        String defaultImage = "/images/not-found.png";
+        try {
+            String url = " https://www.discogs.com/search/?q=" + query + "&type=all";
+            Document doc = Jsoup.connect(url).get();
+
+            //search for the album covers
+            Elements albumCovers = doc.select(".thumbnail_center");
+//            if(albumCovers.size() == 0){
+//                return "not-found.png";
+//            }
+            Element span = albumCovers.get(0);
+            Element img = span.child(0);
+            String src = img.attr("data-src");
+            System.out.println(src);
+            return src;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            return defaultImage;
+        }
+        return defaultImage;
+    }
+}
